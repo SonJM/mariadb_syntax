71f9f806136ac59bd239d0e4a741da5f39bfde88
SonJM <blisle@naver.com>
2023-11-20 17:38:19 +0900

    fix_DDL

--- basic/2.DDL_practice.sql
+++ basic/2.DDL_practice.sql
@@ -55,3 +55,62 @@ WHERE TABLE_NAME = 'post';
 
 -- 외래 키 삭제 (제약 조건은 삭제되나, INDEX는 삭제되지 않는다.)
 ALTER TABLE post DROP FOREIGN KEY post_ibfk_1;
+
+
+---------------------------------------------------------------------
+ALTER TABLE post MODIFY COLUMN id INT AUTO_INCREMENT;
+describe post;
+
+-- post 테이블에 id없이 insert
+-- insert한 데이터 삭제 후 다시 insert
+insert into post(title, contents) values('hello2','world');
+delete from post;
+select * from post;
+alter table post auto_increment = 5;
+insert into post values();
+
+show index from post;
+
+ALTER TABLE post MODIFY COLUMN title varchar(255) UNIQUE;
+ALTER TABLE post ADD CONSTRAINT title UNIQUE(title);
+ALTER TABLE post DROP INDEX title;
+
+SELECT * FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE TABLE_NAME = 'post';
+
+SHOW INDEX FROM post;
+ALTER TABLE post DROP FOREIGN KEY title;
+ALTER TABLE post DROP INDEX title;
+
+ALTER TABLE post ADD CONSTRAINT post_author_fk FOREIGN KEY (author_id) REFERENCES author(id) ON UPDATE CASCADE;
+ALTER TABLE post ADD CONSTRAINT post_author_fk FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE SET NULL ON UPDATE SET NULL;
+
+select * from author;
+select * from post;
+
+show create table post;
+
+select id, title, contents, 
+case author_id
+	when 1 then 'First Author'
+    when 2 then 'Second Author'
+    else 'Others'
+end
+as author_type from post;
+
+-- if문법 사용해서 만약 id가 1이면 first author 그렇지 않으면 others 출력
+-- id, title, contents, author_type
+
+select 
+	case id
+		when 1 then 'first author'
+        else 'others'
+	end as id,
+    title, contents from post;
+    
+select id, title, contents, ifnull(author_id, 'anonymous') from post;
+-- 위에서 사용한 ifnull의 결과값을 동일하게 if문으로 출력
+select id, title, contents, 
+if(author_id is null, 'anonymous', author_id) as author_id from post;
+
+
--- basic/3.DML.sql
+++ basic/3.DML.sql
@@ -12,3 +12,21 @@ SELECT * FROM {table_name} WHERE {조건문} (AND, OR, NOT, NOT IN, ...) {조건
 
 -- DISTINCT KEYWORDS(중복 값을 제외하고 출력)
 SELECT DISTINCT NAME FROM {table_name};
+
+-- 흐름 제어문
+-- CASE - WHEN - END
+SELECT {column_name1}, ...
+CASE {column_name2}
+    WHEN { } THEN { }
+    ...
+    ELSE { }
+END AS {result_name} FROM {table_name};
+
+-- if문법 사용해서 만약 id가 1이면 first author 그렇지 않으면 others 출력
+SELECT id, title, contents, 
+IFNULL(author_id, 'anonymous')
+FROM post;
+-- 위에서 사용한 ifnull의 결과값을 동일하게 if문으로 출력
+SELECT id, title, contents, 
+IF(author_id IS NULL, 'anonymous', author_id)
+AS author_id FROM post;
